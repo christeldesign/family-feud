f23714769b7b5c6e1ab2fc8e2691a8a125753d99
Christel Chan <christelyhchan@gmail.com>
2017-12-01 00:29:37 -0800

    json file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Question from './Question.js';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Family Feud</h1>
+          <h2>BETHEL EDITION</h2>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Question />
         </p>
       </div>
     );
---
+++ src/Question.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Question extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className = 'question'>Question</h1>
+
+        <div className = 'choice 1'>
+
+        </div>
+
+        <div className = 'choice 2'>
+
+        </div>
+
+        <div className = 'choice 3'>
+
+        </div>
+
+        <div className = 'choice 4'>
+
+        </div>
+
+        <div className = 'choice 5'>
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Question;
